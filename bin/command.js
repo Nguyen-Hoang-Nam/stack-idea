8716143ea1ad7fb62385ef935c86f08c6ef08565
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-01-30 22:43:19 +0700

    ➕ Add CHANGELOG, update README
    _ Add CHANGELOG version 1.2.0
    _ Add more screenshot
    _ Minimize image size
    _ Add more example for manipulate stack-config
    _ Add new command to help

--- bin/command.js
+++ bin/command.js
@@ -76,6 +76,16 @@ exports.help = isHelp => {
 	helpCommand('-u, --untick', 'Untick when setup are not done yet');
 	helpCommand('-r, --remove', 'Remove tech that not use');
 	helpCommand('-G, --global', 'Use file stack.json in global');
+	helpCommand('-y, --yaml', 'Use yaml file type instead of json');
+	helpCommand('-i, --item ', 'Use to input array of parameter');
+	helpCommand('--add-item', 'Add item to row in stack-config in global');
+	helpCommand('--remove-item', 'Remove item from row in stack-config in global');
+	helpCommand('--get-row', 'Print all items of row in stack-config in global');
+	helpCommand('--add-row', 'Add row to stack-config in global');
+	helpCommand('--remove-row', 'Remove row from stack-config in global');
+	helpCommand('--hide-row', 'Hide row in stack-config in global');
+	helpCommand('--show-row', 'Show row that hide in stack-config in global');
+	helpCommand('--get-all', 'Print stack-config in global');
 	console.log('\n');
 	console.log('Examples: \n');
 	console.log('  $ stack --generate --show');
